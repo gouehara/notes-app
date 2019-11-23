14ecd83fd457ab6af379672c97212d8c54d8cde0
gouehara <ueharanaha@gmail.com>
2019-11-23 20:08:08 +0900

    update

--- scripts/notes-app.js
+++ scripts/notes-app.js
@@ -21,7 +21,7 @@ document.querySelector('#create-note').addEventListener('click', () => {
     updatedAt: timestamp
   })
   saveNotes(notes)
-  location.assign(`/edit.html#${id}`)
+  location.assign(`/edit.html`)
 })
 
 document.querySelector('#search-text').addEventListener('input', e => {
